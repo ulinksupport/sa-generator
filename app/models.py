dd44e0ce4a437406b6485e17d87722a22652f21c
Your Name <supportdesk@ulinkassist.com>
2026-03-23 15:35:14 +0800

    Fix UniqueConstraint import

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, func
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, func, UniqueConstraint
 from sqlalchemy.orm import relationship
 from .db import Base
 
